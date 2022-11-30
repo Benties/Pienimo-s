58dfec99d69b9e378726fa4b396003f4c6dd577c
Benjamin Thai <ben.thai@aol.com>
2022-11-29 22:33:34 -0500

    fixed maybe??

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
 from .pie import Pie
 from .order import Order
 from .address import Address
-from .db import environment, SCHEMA
